ff454d517f1d546640a54b22b78e3bb3b7c774d4
Vyacheslav Nagel <v9gel@mail.ru>
2021-07-24 13:56:18 +0700

    * блок принимает параметры с координатами и тайтлом

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      <Block />
+      <Block title={1} position={{ x: 0, y: 0 }}></Block>
     </div>
   );
 }
--- src/Block.tsx
+++ src/Block.tsx
@@ -1,16 +1,26 @@
 import React, { useState } from "react";
 import "./Block.css";
 
-export const Block = () => {
+export const Block = (props: {
+  position: { x: number; y: number };
+  title: number;
+}) => {
   const [position, setPosition] = useState(0);
 
   return (
     <div
       className="block"
       onClick={() => setPosition((position) => position + 1)}
-      style={{ transform: "translate(" + position * 100 + "px, 100px)" }}
+      style={{
+        transform:
+          "translate(" +
+          props.position.x * 100 +
+          "px, " +
+          props.position.y * 100 +
+          "px)",
+      }}
     >
-      {position}
+      {props.title}
     </div>
   );
 };
